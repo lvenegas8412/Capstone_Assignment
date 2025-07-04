e31edcad7e4517d8a56b6e20630892e78916d3ad
Luis Venegas <lvenegas8412@gmail.com>
2025-07-03 17:50:21 -0700

    fix grid expansion

--- gui_window.py
+++ gui_window.py
@@ -13,28 +13,34 @@ root.geometry('1200x800')
 root.title('Capstone Project - Weather Dashboard')
 root.attributes('-topmost', True)
 root.configure(bg='white')
-root.grid_columnconfigure(0, weight=1)
+
+root.grid_columnconfigure(1, weight=1) 
+    
+
 
 #Title Window
 title_label = tk.Label(
     root,
     text = 'Lets Find Some Weather!',
-    font = ('Courier', 24),
+    font = ('Courier', 24, 'bold'),
     bg = 'white',
     fg = 'black')
-title_label.grid(row=0, column=0, sticky='we', pady=10 )
+title_label.grid(row=0, column=0, sticky='nwes', pady=10 )
 
 #Input_Frame
-input_frame = ttk.LabelFrame(
+input_frame = tk.LabelFrame(
     root,
     text='Input',
-    padding=10)
+    bg='light gray')
 input_frame.grid(
     row=1, 
     column=0, 
-    sticky='w', 
+    sticky='nwes', 
     pady=10, 
-    padx=10)
+    padx=10
+    )
+
+
 
 #Input_Label
 input_label = ttk.Label(
@@ -42,7 +48,10 @@ input_label = ttk.Label(
     text='Enter your City here:')
 input_label.grid(
     row=0, 
-    column=0, sticky='w')
+    column=0, 
+    sticky='w',
+    pady=10
+    )
 
 #Input_Entry
 entry_var = tk.StringVar(value='--')
@@ -57,7 +66,10 @@ drop_box = ttk.Combobox(
 )
 
 drop_box.current(0) #default
-drop_box.grid(row=1, column=1, sticky='w', pady=5, padx=5)
+drop_box.grid(row=1, 
+              column=1, 
+              sticky='w', 
+              pady=5, padx=5)
 
 #Drop_Box_Label
 drop_box_label = ttk.Label(
@@ -65,15 +77,30 @@ drop_box_label = ttk.Label(
     text='Time Range:').grid(
     row=1, column=0,
     sticky = 'w',
-    pady=5
+    pady=10
       )
 
-# style = ttk.Style()
-# style.configure('Custom.TLabelframe', background='gray')
-# style.configure('Custom.TLabelframe.Label', background='gray')  # For the label text bg
+#Current_Weather_Frame
+weather_frame = tk.LabelFrame(
+    root,
+    text='Current Weather Conditions',
+    padx=10,
+    bg='light gray')
+weather_frame.grid(
+    row=1,
+    column=1,
+    sticky='nwes',
+    pady=10,
+    padx=10)
 
-# input_frame = ttk.LabelFrame(root, text='INPUT', padding=10, style='Custom.TLabelframe')
 
+#Current_Weather_Labels
+temp_label = tk.Label(
+    weather_frame,
+    text='Temperature: --')
+temp_label.grid(row=0, column=0, 
+                sticky='w', 
+                pady=5)
 
 
 

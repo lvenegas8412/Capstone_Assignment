ebe394abf4cd2007bea5ad7e0e9f1dbd2d187253
lvenegas8412 <lvenegas8412@gmail.com>
2025-07-27 18:16:09 -0700

    commented out config

--- config.py
+++ config.py
@@ -28,33 +28,33 @@ else:
     print(response.text)
 
 
-# WEATHERBIT API
-end_date = datetime.now().date()
-start_date = end_date - timedelta(days=1)
-start_date_str = start_date.strftime('%Y-%m-%d')
-end_date_str = end_date.strftime('%Y-%m-%d')
+# # WEATHERBIT API
+# end_date = datetime.now().date()
+# start_date = end_date - timedelta(days=1)
+# start_date_str = start_date.strftime('%Y-%m-%d')
+# end_date_str = end_date.strftime('%Y-%m-%d')
 wbapi_key = os.getenv('weatherbit_api')
 
 weatherbit_api_url = "https://api.weatherbit.io/v2.0/history/daily"
 
-# Parameters for our request
-wb_parameters = {
-    "city": "New York",
-    "state": "NY",
-    "country": "US",
-    "start_date": start_date_str,
-    "end_date": end_date_str, 
-    "key": wbapi_key,
-    "units": "I"
-}
+# # Parameters for our request
+# wb_parameters = {
+#     "city": "New York",
+#     "state": "NY",
+#     "country": "US",
+#     "start_date": start_date_str,
+#     "end_date": end_date_str, 
+#     "key": wbapi_key,
+#     "units": "I"
+# }
 
-# Make the request
-wb_response = requests.get(weatherbit_api_url, params=wb_parameters)
+# # Make the request
+# wb_response = requests.get(weatherbit_api_url, params=wb_parameters)
 
-# Check if request was successful and get data
-if wb_response.status_code == 200:
-    wb_weather_data = wb_response.json()
+# # Check if request was successful and get data
+# if wb_response.status_code == 200:
+#     wb_weather_data = wb_response.json()
 
-else:
-    print(f"Error: {wb_response.status_code}")
-    print(wb_response.text)
+# else:
+#     print(f"Error: {wb_response.status_code}")
+#     print(wb_response.text)
